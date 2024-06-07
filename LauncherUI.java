029a6eb173dbed1af08b0f4cf9fc46d342bc11b7
OneAvargeCoder193 <mgiakimenko@outlook.com>
2024-06-06 23:03:25 -0400

    Fix

--- LauncherUI.java
+++ LauncherUI.java
@@ -4,6 +4,12 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Locale.Category;
+
+import github.Github;
+import github.Repository;
+import github.Commit;
 
 public class LauncherUI {
     private JFrame frame;
@@ -14,20 +20,41 @@ public class LauncherUI {
     private JButton updateButton;
     private JButton playButton;
     private JCheckBox debugCheckbox;
+    private JComboBox<String> versionList;
+
+    private Repository cubyz;
 
     public void createAndShowGUI() {
         ThemeSetter.setTheme();
+        initializeGithub();
         initializeComponents();
         setupLayout();
         setupActions();
         frame.setVisible(true);
     }
 
+    private void initializeGithub() {
+        cubyz = Github.getRepo("PixelGuys", "Cubyz");
+    }
+
     private void initializeComponents() {
         frame = new JFrame("Cubyz Launcher");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 400);
+        
+        String fcommits[] = null;
+        try {
+            fcommits = cubyz.getCommits().stream().map(Commit::getShortSha).toArray(String[]::new);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        String commits[] = new String[fcommits.length + 1];
+        commits[0] = "master";
+        System.arraycopy(fcommits, 0, commits, 1, fcommits.length);
+
+        versionList = new JComboBox<>(commits);
+        
         outputConsole = new JTextArea();
         outputConsole.setEditable(false);
 
@@ -56,17 +83,18 @@ public class LauncherUI {
         JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         buttonPanel.add(fetchButton);
         buttonPanel.add(updateButton);
+        buttonPanel.add(versionList);
 
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.gridwidth = 2;
+        gbc.gridwidth = 3;
         frame.add(buttonPanel, gbc);
 
         JPanel topRightPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         topRightPanel.add(playButton);
         topRightPanel.add(debugCheckbox);
 
-        gbc.gridx = 2;
+        gbc.gridx = 3;
         gbc.gridy = 0;
         gbc.gridwidth = 1;
         frame.add(topRightPanel, gbc);
@@ -99,7 +127,8 @@ public class LauncherUI {
             public void actionPerformed(ActionEvent e) {
                 new Thread(() -> {
                     try {
-                        String url = "https://github.com/PixelGuys/Cubyz/archive/refs/heads/master.zip";
+                        String commit = (String)(versionList.getSelectedItem());
+                        String url = "https://github.com/PixelGuys/Cubyz/archive/" + commit + ".zip";
                         String destDir = "./.cubyz/fetched/";
                         Downloader.downloadAndUnzip(url, destDir, outputConsole, progressBar, fetchingLabel);
                     } catch (IOException ex) {
